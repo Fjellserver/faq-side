2ef6364881777546cb480280f33fa7ada8000fd2
BuildTools <unconfigured@null.spigotmc.org>
2021-03-03 20:06:34 +0100

    footer fix

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -91,7 +91,7 @@
             </div>
         </div>
     </div>
-        <div class="copyright py-4 text-center text-white" style="height: auto;background-color: #2c3e50;">
+        <div class="copyright py-4 text-center text-white" style="background-color: #2c3e50;">
         <div class="container"><small id="year">Opphavsrett © Fjellserver 2019 -&nbsp;</small><br><small>Fjellserver.no er på ingen måte tilknyttet/levert/drevet/støttet av Mojang AB eller Microsoft.</small></div>
     </div>
 </footer>
